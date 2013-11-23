06fcd508cb32fc258111654e9507a74539686bb2
Tommi <me@tommi.co>
2013-11-24 00:44:07 +0800

    Able to save images to S3!

--- web.rb
+++ web.rb
@@ -5,10 +5,26 @@ RACK_ENV ||= ENV["RACK_ENV"] || "development"
 require 'bundler/setup'
 require 'sinatra'
 require "sinatra/reloader" if development?
-
+require 'Base64'
+require 'aws/s3'
+include AWS::S3
 
 get '/' do
 	send_file File.expand_path('index.html', settings.public_folder)
+end
+
+post '/upload_tile' do
+	decoded_image = Base64.decode64(params['imgBase64'])
+
+	AWS::S3::DEFAULT_HOST.replace('s3-ap-southeast-1.amazonaws.com') 
+
+	AWS::S3::Base.establish_connection!(
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    )
+
+    S3Object.store("#{Time.now.to_f}.png", decoded_image, ENV['S3_BUCKET'],
+                   access: :public_read)
 end
 
 get '/ping' do
