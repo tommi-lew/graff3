5994c9801b2e3ef31c14500bd3f208ff7c678e06
Tommi <me@tommi.co>
2013-11-24 02:50:27 +0800

    It's coming alive…!

--- web.rb
+++ web.rb
@@ -24,6 +24,42 @@ get '/' do
 	send_file File.expand_path('index.html', settings.public_folder)
 end
 
+get '/canvas' do
+	html = ""
+
+	0.upto(4) do |row|
+		0.upto(4) do |col|
+			current_key = "#{row}-#{col}"
+
+			current_tile = redis.hmget('c1', current_key)[0]
+
+			src = "Graffcubed logo.png"
+
+			if current_tile != nil
+				src = ENV['S3_HOST'] + "/" +  current_tile
+			end
+
+
+			if (col == 0)
+				html += '<div class="ui-block-a"><img src="'+ src +'" ></div>'
+			elsif (col == 1)
+				html += '<div class="ui-block-b"><img src="'+ src +'" ></div>'
+			elsif (col == 2)
+				html += '<div class="ui-block-c"><img src="'+ src +'" ></div>'
+			elsif (col == 3)
+				html += '<div class="ui-block-d"><img src="'+ src +'" ></div>'
+			elsif (col == 4)
+				html += '<div class="ui-block-e"><img src="'+ src +'" ></div>'
+			end
+			
+		end
+	end
+
+	puts html
+
+	erb :'grid.html', :locals => {:grids => html}
+end
+
 post '/upload_tile' do
 	decoded_image = Base64.decode64(params['imgBase64'])
 
